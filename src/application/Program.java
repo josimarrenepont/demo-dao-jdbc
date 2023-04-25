5cae4852d132fec4e4762da8dd9eb60f1c7ee507
Josimar Renepont <jnrenepont@hotmail.com>
2023-04-24 21:19:40 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,18 +1,25 @@
 package application;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 	
-	Department dep = new Department(1, "books");
-	System.out.println(dep);
+	SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	
+	Department obj = new Department(1, "books");
+	
+	Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.00, obj);
+	
+	System.out.println(seller);
 	
 	
-	
-	
-	
+
 	}
 
 }
