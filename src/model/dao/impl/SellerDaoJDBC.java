17f976e9389ba089337628eebb2e99c81f7a7e1b
Josimar Renepont <jnrenepont@hotmail.com>
2023-04-26 21:21:11 -0300

    update implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -60,6 +60,29 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public void update(Seller obj) {
+		PreparedStatement st = null;
+
+		try {
+			st = conn.prepareStatement(
+					"UPDATE seller "
+					+"SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? "
+					+"WHERE Id = ?");
+
+			st.setString(1, obj.getName());
+			st.setString(2, obj.getEmail());
+			st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+			st.setDouble(4, obj.getBaseSalary());
+			st.setInt(5, obj.getDepartment().getId());
+			st.setInt(6, obj.getId());
+
+			st.executeUpdate();
+		}
+		 catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+
+		}
 	}
 
 	@Override
@@ -92,7 +115,7 @@ public class SellerDaoJDBC implements SellerDao {
 	}
 
 	private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException { // PROPAGAR EXCEÇÃO
-																							// SQLEXCEPTION
+																						// SQLEXCEPTION
 
 		Seller obj = new Seller();
 		obj.setId(rs.getInt("Id"));
